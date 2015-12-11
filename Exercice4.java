2b465896aae5bc93f9c9f30046cfa5cddb5d09a5
Karin Touchie <ktouchie@gmail.com>
2015-12-11 16:27:23 +0100

    Add first loop (Le pas croissant)

---
+++ Exercice4.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class Exercice4 {
+    
+    public static void main(String [] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Veuillez saisir un nombre entier positif:");
+            int n = sc.nextInt();
+        
+        System.out.println("Le pas croissant jusqu'a " + n + ":");
+        for (int i=1; i<=n; i+(i-1)) {
+            System.out.println(i);
+        };             
+    }
+}
